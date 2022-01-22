7bbfa1144c5909339b17f25f7a98083bc1c96e26
Sabine Tait <sabine.tait@gmail.com>
2022-01-22 17:34:25 -0500

    add nav and home page components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
 import ResturantSearchPage from './ResturantSearchPage';
+import { Routes, Route } from 'react-router-dom';
+
+import NavBar from './NavBar.js';
+import HomePage from './HomePage.js';
 
 function App() {
   
   return (
     <div className="App">
+      <NavBar />
+
       <header className="App-header">
-       <ResturantSearchPage/>
+       
+       <Routes>
+          <Route path="/" element={ <HomePage/>}/>
+          <Route path="/restaurant-search" element={<ResturantSearchPage/> }/>
+       </Routes>
       </header>
     </div>
   );
