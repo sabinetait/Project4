74e3ad67f416f20e8c480e44c2ee6ac44d9e27e5
Rachel <rh.rachelholloway@gmail.com>
2022-01-23 22:21:06 -0500

    attempted to nest data in firebase :fire:

--- src/FirebaseHandlingData.js
+++ src/FirebaseHandlingData.js
@@ -1,5 +1,5 @@
 import firebase from './Firebase';
-import { getDatabase, ref, onValue, push, remove } from 'firebase/database';
+import { getDatabase, ref, onValue, push, remove, set } from 'firebase/database';
 import { useState, useEffect } from 'react';
 
 const FirebaseHandlingData = () => {
@@ -12,7 +12,6 @@ const FirebaseHandlingData = () => {
   const [Prompt, setPrompt] = useState(false); 
   const [Fire, setFire] = useState(''); 
     
-
   useEffect(() => {
     onValue(dbRef, (response) => {
       const newState = [];
@@ -28,6 +27,14 @@ const FirebaseHandlingData = () => {
   const handleInputChange = (event) => {
       setUserInput(event.target.value);
   };
+
+  const writeUserData = () => {
+    let Rest = ["hi"];
+    const db = getDatabase();
+    set(ref(db, 'city/'), {
+      Rest
+    });
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -48,27 +55,50 @@ const FirebaseHandlingData = () => {
     setPrompt(false)
     setFire('');
   };
+
+  const renderId = () => {
+    if (trips.id === null || trips.id === "" || trips.id === undefined || trips.id.length === 0);
+   else {
+     return (
+       <div>
+       </div>
+     )
+   }
+  }
+
+  const renderTrips = () => {
+    if (trips === null || trips === "" || trips === undefined || trips.length === 0);
+  else {
+    return (
+      <div>
+        <ul>
+          {[trips].map((trip) => {
+            // {renderId()} 
+            return (
+              <li key={trip.key}>
+                <p>{trip.name}</p>
+                <button onClick={() => { setPrompt(true); setFire(trip.key) }}>Click to Remove</button>
+              </li>
+            );
+          })}
+        </ul>
+  
+      </div>
+    )
+  }
+  }
   
   return (
     <>
       <div className="wrapper">
+        {renderTrips()}
         
-        <ul>
-          {trips.map((trip) => {
-            return (
-                <li key={trip.key}>
-                  <p>{trip.name}</p>
-                  <button onClick={() => { setPrompt(true); setFire(trip.key)}}>Click to Remove</button>  
-                </li>
-              ); 
-            })}
-        </ul>
-
         <form action="submit">
             <label htmlFor="newTrip" aria-label="Add new trip"></label>
             <input placeholder="Add a new trip" type="text" id="newTrip" value={userInput} onChange={handleInputChange} />
             <button onClick={handleSubmit}>Add a trip</button>
         </form>
+        <button onClick={writeUserData}>Click me</button>
           
         <div className={`PromptMenu${Prompt ? " opened" : " closed"}`}>
           <button onClick={() => setPrompt(false)}>Go Back</button>
