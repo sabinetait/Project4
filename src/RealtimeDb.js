b161064d484049d1bc1c7fea952b0c605ef6442c
Sabine Tait <sabine.tait@gmail.com>
2022-01-25 15:33:21 -0500

    button and prompt to remove restaurant from trip

--- src/RealtimeDb.js
+++ src/RealtimeDb.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react';
 import firebase from './Firebase';
 import { getDatabase, onValue, ref, set, remove, push } from 'firebase/database';
+import { dblClick } from '@testing-library/user-event/dist/click';
 const RealtimeDb = () => {
     const database = getDatabase(firebase);
     const [productList, setProductList] = useState([]);
     const [fire, setFire] = useState("");
     const [userInput, setUserInput] = useState('');
+    const [prompt, setPrompt] = useState(false);
     
     useEffect(() => {
-        const dbRootAddress = ref(database, 'City/');
+        const dbRootAddress = ref(database, 'City/toronto/Restaurant/');
+
         onValue(dbRootAddress, (response) => {
             if (response.val() === null) {
                 setProductList([])
@@ -37,8 +40,7 @@ const RealtimeDb = () => {
                         <>
                             <li id={`${key}`} key={index}>
                                 <p >{productList[`${key}`].name}</p>
-                                <p >{productList[`${key}`].city}</p>
-                                <p >{productList[`${key}`].restaurant}</p>
+                                <img src={productList[`${key}`].image} alt={productList[`${key}`].name} />
                                 <button value={`${key}`} onClick={(event) => { setFire(`${event.target.value}`); handleRemoveTrip()}}>Click to Remove</button>
                             </li>
                         
@@ -51,12 +53,22 @@ const RealtimeDb = () => {
 
     const handleRemoveTrip = () => {
         console.log(fire);
-        const databaseReference = ref(database, `City/${fire}`)
-        remove(databaseReference);
+        setPrompt(true);
+
+
+        // const databaseReference = ref(database, `City/${fire}`)
+        // remove(databaseReference);
     };
     
     const handleInputChange = (event) => {
         setUserInput(event.target.value);
+    };
+
+    const handleRemoveRestaurant = () => {
+        console.log(fire);
+        const databaseReference = ref(database, `City/toronto/Restaurant/${fire}`);
+        remove(databaseReference);
+        setPrompt(false);
     };
 
     return (
@@ -68,6 +80,14 @@ const RealtimeDb = () => {
                 <input placeholder="Add a new trip" type="text" id="newTrip" value={userInput} onChange={handleInputChange} />
                 <button onClick={writeUserData}>Add a trip</button>
             </form>
+
+            <div className={`prompt${prompt? " opened" : " closed"}`}>
+                <p>Are you sure you want to remove this restaurant?</p>
+                <button onClick={() => { handleRemoveRestaurant()}}>Yes, remove</button>
+                <button onClick={() => setPrompt(false)}>No, go back</button>
+            </div>
+             
+            
         </>
     )
 }
